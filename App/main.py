51ceed30b84e5df392ff27b0350787886fbcf6ba
Sumit Tiwari <135542949+mesumittiwari@users.noreply.github.com>
2025-04-20 14:55:31 +0530

    Update main.py

--- App/main.py
+++ App/main.py
@@ -1,7 +1,8 @@
-# main.py
-
-# Copyright [Year] Sumit Tiwari
-# [Optional: Add license information here, e.g., MIT License]
+# Required for ChromaDB compatibility on Streamlit Cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# --- End of ChromaDB patch ---
 
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
