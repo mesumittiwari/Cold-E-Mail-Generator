2291e69d5cb4106b952de318144186dcc5c5b478
Sumit Tiwari <135542949+mesumittiwari@users.noreply.github.com>
2025-04-20 15:03:37 +0530

    Update main.py

--- App/main.py
+++ App/main.py
@@ -2,6 +2,13 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 # --- End of ChromaDB patch ---
 
 import streamlit as st
